94143e1627410759ceb369d7703d423f6b649dc7
everdro1d <everdro1d-github@pm.me>
2025-05-03 09:14:07 -1000

    ""

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
