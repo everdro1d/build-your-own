4fa416050dce7c714bd84f4b87ad3731f771739d
everdro1d <everdro1d-github@pm.me>
2025-05-03 08:40:50 -1000

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,9 @@ public class Main {
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
