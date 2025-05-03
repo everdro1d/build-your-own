df8f756a48b8d1f7397ca1dab5f60d6eb1289d34
everdro1d <everdro1d-github@pm.me>
2025-05-03 08:46:53 -1000

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            System.out.print("$ ");
+            if (input.isEmpty()) {
+                break;
+            }
             System.out.println(input + ": command not found");
         }
     }
